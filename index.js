a2594f71bd6b3195504a81050060613832e564c0
MaazDanish <dk599318@gmail.com>
2023-07-25 16:51:35 +0530

    update the items

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ header.style.borderBottom = ' solid 5px #ff0700';
 let addItems = document.querySelector('.title');
 addItems.style.fontWeight = 'bold';
 addItems.style.color = 'green';
+
+let item = document.querySelectorAll('.list-group-item');
+console.log(item[2]);
+item[2].style.color = 'green';
+item.style.fontWeight = 'bolder';
+
