a5c08aae01e6b5438a6a23771b6054fbc21b6bfa
MaazDanish <dk599318@gmail.com>
2023-07-25 16:29:20 +0530

    changed the title

---
+++ index.js
@@ -0,0 +1,7 @@
+document.title = 'Title Changed';
+let header = document.querySelector('#main-header');
+header.style.borderBottom = ' solid 5px #ff0700';
+
+let addItems = document.querySelector('.title');
+addItems.style.fontWeight = 'bold';
+addItems.style.color = 'green';
